1c204b981266815c1fb2daf14aeb4de4c693d779
Harry <hnicholls@rbi.com>
2023-11-13 10:15:18 -0500

    Changes deps and add some code

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Hi");
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Harry</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
